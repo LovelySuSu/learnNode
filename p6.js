0d4ece30fb981b7093b779e50d4a91980cd31df5
许丁韩杨 <XUDINGHANYANG110@pingan.com.cn>
2018-02-26 17:46:14 +0800

    模块化

--- p6.js
+++ p6.js
@@ -1,12 +1,12 @@
 /**
  * Created by dingding on 2018/2/9.
  */
-var Read = require('./pmodule');
+var read = require('./pmodule');
 var process = require('process');
-var read = new Read(process.argv[2],process.argv[3],function callback(err,data){
-    if(err) return console.error('There was an error:', err)
+
+read(process.argv[2],process.argv[3],function callback(err,data) {
+    if (err) return console.log(err);
     else data.forEach(function (val) {
         console.log(val);
-    });
-});
-read.readFile();
+    })
+})
--- pmodule.js
+++ pmodule.js
@@ -1,16 +1,16 @@
 /**
  * Created by dingding on 2018/2/9.
  */
-function Read(dir,tail,fn){
-    var fs = require('fs');
-    this.readFile = function () {
-        fs.readdir(dir,function callback(err,data){
-            if(err) return fn(err);
-            else {
-                let reg = new RegExp('^.*.'+tail+'$','i');
-                return fn(null,data.filter(val=>reg.test(val)))
-            }
-        })
-    }
+var fs = require('fs')
+var path = require('path')
+
+function readFile(dir, tail, fn) {
+    fs.readdir(dir, function (err, data) {
+        if (err) return fn(err);
+        else{
+            let reg = new RegExp('^.*.'+tail+'$','i')
+            fn(null,data.filter(val=>reg.test(val)));
+        }
+    })
 }
-module.exports = Read;
+module.exports = readFile;
